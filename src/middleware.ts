540bedcabc4d4a73fe024ff856ee06ca155d2d96
fhenri <fhenri@fhsolution.com>
2024-11-12 18:25:40 +0100

    add middleware to manage all routes authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,24 @@
-export { auth as middleware } from "@/lib/auth"
+import { auth as middleware } from "@/lib/auth";
+
+export default middleware((req) => {
+    const restrictedRoutes = [
+        '/admin/',
+    ]
+
+    const restrictedAPIRoutes = [
+        '/api/admin/',
+    ]
+
+    const isRestrictedRoutes = !!restrictedRoutes.find(route => req.nextUrl.pathname.startsWith(route.toLowerCase()));
+
+    if (!req.auth && isRestrictedRoutes) {
+        const newUrl = new URL("/admin", req.nextUrl.origin);
+        return Response.redirect(newUrl);
+    }
+
+    const isRestrictedAPIRoutes = !!restrictedRoutes.find(route => req.nextUrl.pathname.startsWith(route.toLowerCase()));
+    if (!req.auth && isRestrictedRoutes) {
+        return Response.json({ message: "Not authenticated" }, { status: 401 })
+    }
+
+});
